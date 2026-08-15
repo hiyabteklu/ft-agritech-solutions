97e18948e06210ec2e8b035b48ad9477919888db
Hiyab Teklu <205397659+hiyabteklu@users.noreply.github.com>
2026-08-15 19:55:13 +0300

    Update Supabase URL in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://qxdfuddoznqrkgkbylgu.supabase.co';
+const SUPABASE_URL = 'https://qdfuddoznqlrkgkbylgu.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_JiydRHQ3JZXfFeJXzgCkdw_AlGmJtBy';
 
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
